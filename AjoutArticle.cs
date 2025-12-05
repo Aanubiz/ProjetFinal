f3d5b3e2224cf7df506a361ace4e380dd16120d6
66230 <662301497david@gmail.com>
2025-12-04 20:49:22 -0500

    verification

--- AjoutArticle.cs
+++ AjoutArticle.cs
@@ -9,7 +9,7 @@ namespace ProjetFinal
       
       /*--------- Déclaration de la liste des articles --------------*/
       List<(string code, string nom, decimal prix)> articles = new ();
-      articles.Add(("A1", "Crayons", 3.99M));
+      articles.Add(("A1", "Crayon", 3.99M));
       articles.Add(("A2", "Cahier Canada", 1.59M));
       articles.Add(("B1", "Table Pliante", 66.99M));
       articles.Add(("B2", "Fauteuil en cuir", 199.99M));
